2507d8eb5157afc5bda6fbe2de0800700f0eabe3
Karan Gupta <gkaran184@gmail.com>
2023-03-30 22:28:33 +0530

    Added thunk type to Store
    Signed-off-by: Karan Gupta <gkaran184@gmail.com>

---
+++ src/main/java/org/flux/store/api/Thunk.java
@@ -0,0 +1,9 @@
+package org.flux.store.api;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public interface Thunk<T extends State> {
+
+    void process(Consumer<Action> next, Supplier<T> getState);
+}
