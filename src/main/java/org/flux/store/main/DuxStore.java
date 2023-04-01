4b834315dce5abfbe602d3da414c24571d663f04
Karan Gupta <gkaran184@gmail.com>
2023-04-01 07:08:30 +0530

    Signed-off-by: Karan Gupta <gkaran184@gmail.com>

--- src/main/java/org/flux/store/main/DuxStore.java
+++ src/main/java/org/flux/store/main/DuxStore.java
@@ -9,7 +9,6 @@ import java.util.function.Consumer;
 
 public class DuxStore<T extends State> implements Store<T> {
 
-    public static final String INITIAL_ACTION = "STORE_INITIALIZATION";
     private Reducer<T> reducer;
     private TimeTravel<T> timeTravel;
     private T state;
@@ -20,7 +19,7 @@ public class DuxStore<T extends State> implements Store<T> {
         this.state = initialState;
         this.reducer = reducer;
         this.timeTravel = new TimeTravel<>();
-        this.timeTravel.recordChange(INITIAL_ACTION, initialState);
+        this.timeTravel.recordChange(new Action<>(Utilities.INITIAL_ACTION, initialState));
     }
 
     public DuxStore(T initialState, Reducer<T> reducer, Middleware<T> middleware) {
@@ -53,8 +52,21 @@ public class DuxStore<T extends State> implements Store<T> {
         boolean isChanged = diffResult.getNumberOfDiffs() > 0;
         if(isChanged) {
             this.state = newState;
-            this.timeTravel.recordChange(action.getType(), newState);
+            this.timeTravel.recordChange(action);
             this.notifyListeners();
+        }
+    }
+
+    private void dispatchNoNotify(Action action) {
+        T newState = reducer.reduce(action, (T) state.clone());
+        this.state = newState;
+    }
+
+    private void dispatchTimeTravel(Action action) {
+        if(middleware != null) {
+            middleware.run(this, this::dispatchNoNotify, action);
+        } else {
+            dispatchNoNotify(action);
         }
     }
 
@@ -74,17 +86,24 @@ public class DuxStore<T extends State> implements Store<T> {
 
     public void goBack() {
         this.timeTravel.goBack();
-        this.state = this.timeTravel.getCurrentState();
+        T initialState = this.timeTravel.getInitialState();
+        this.state = initialState;
+        List<Action> actionsToRecreateState = this.timeTravel.getActionHistory();
+        for (Action action: actionsToRecreateState) {
+            dispatchTimeTravel(action);
+        }
         this.notifyListeners();
     }
 
     public void goForward() {
         this.timeTravel.goForward();
-        this.state = this.timeTravel.getCurrentState();
+        Action latestAction = this.timeTravel.getLatestAction();
+        T newState = reducer.reduce(latestAction, (T) state.clone());
+        this.state = newState;
         this.notifyListeners();
     }
 
     public List<String> getActionHistory() {
-        return this.timeTravel.getActionHistory();
+        return this.timeTravel.getActionTypeHistory();
     }
 }
