19a85bfd7af7ea56860e3da0d3aa01b1d13b230c
Karan Gupta <gkaran184@gmail.com>
2023-04-04 14:19:57 +0530

    Signed-off-by: Karan Gupta <gkaran184@gmail.com>

--- src/main/java/org/flux/store/main/DuxStore.java
+++ src/main/java/org/flux/store/main/DuxStore.java
@@ -3,10 +3,12 @@ package org.flux.store.main;
 import com.google.gson.Gson;
 import org.apache.commons.lang3.builder.DiffResult;
 import org.flux.store.api.*;
+import org.flux.store.kafka.Producer;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.function.Consumer;
 
 public class DuxStore<T extends State> implements Store<T> {
@@ -16,12 +18,15 @@ public class DuxStore<T extends State> implements Store<T> {
     private T state;
     public List<Consumer<T>> listeners = new ArrayList<>();
     private Middleware<T> middleware;
+    private Producer producer;
+    private Gson gson = new Gson();
 
     public DuxStore(T initialState, Reducer<T> reducer) {
         this.state = initialState;
         this.reducer = reducer;
         this.timeTravel = new TimeTravel<>();
         this.timeTravel.recordChange(new Action<>(Utilities.INITIAL_ACTION, initialState));
+        Runtime.getRuntime().addShutdownHook(new Thread(this::killProducer));
     }
 
     public DuxStore(T initialState, Reducer<T> reducer, Middleware<T> middleware) {
@@ -110,14 +115,36 @@ public class DuxStore<T extends State> implements Store<T> {
     }
 
     public String exportStore() {
-        Gson gson = new Gson();
-        return gson.toJson(this.state);
+        return this.gson.toJson(this.state);
     }
 
     public void importStore(String json, Type type) {
-        Gson gson = new Gson();
-        T state = gson.fromJson(json, type);
+        T state = this.gson.fromJson(json, type);
         this.state = state;
         this.notifyListeners();
+    }
+
+    public void initializeKafkaProducer(Properties properties, String topic) {
+        try {
+            this.producer = new Producer(properties, topic);
+            this.subscribe(x -> this.sendMessageToKafka());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void killProducer() {
+        if(this.producer != null)
+            this.producer.kill();
+    }
+
+    private void sendMessageToKafka(){
+        try {
+            if (producer != null) {
+                producer.sendMessage(this.exportStore());
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
---
+++ src/test/java/org/flux/store/tests/userprofile/KafkaTest.java
@@ -0,0 +1,67 @@
+package org.flux.store.tests.userprofile;
+
+import org.flux.store.api.Action;
+import org.flux.store.api.Reducer;
+import org.flux.store.main.DuxStore;
+import org.flux.store.main.Utilities;
+import org.flux.store.tests.userprofile.domain.Author;
+import org.flux.store.tests.userprofile.domain.Book;
+import org.flux.store.tests.userprofile.domain.CombinedState;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class KafkaTest {
+
+    public static final String INITIAL_BOOK = "The Making of a manager";
+    public static final String INITIAL_AUTHOR = "Julie Zhou";
+    public static final String ACTION_SET_BOOK = "SET_BOOK";
+    public static final String ACTION_SET_AUTHOR = "SET_AUTHOR";
+
+    private DuxStore<CombinedState> myStore;
+
+    @BeforeEach
+    public void init() {
+
+        CombinedState initialState = new CombinedState(new Book(INITIAL_BOOK),new Author(INITIAL_AUTHOR));
+
+        Reducer<CombinedState> reducer = (action, state) -> {
+            switch (action.getType()) {
+                case ACTION_SET_AUTHOR:
+                    Author author = (Author) action.getPayload();
+                    state.setAuthor(author);
+                    break;
+                case ACTION_SET_BOOK:
+                    Book book = (Book) action.getPayload();
+                    state.setBook(book);
+                    break;
+            }
+            return state;
+        };
+        myStore = new DuxStore<>(initialState, reducer);
+        myStore.subscribe(System.out::println);
+    }
+
+    @Test
+    public void canSendFullFeedToKafkaTopic() {
+        Properties properties = new Properties();
+        String jaasConfig = System.getProperty("jaas.config");
+        String cluster = System.getProperty("bootstrap.servers");
+        String topicName = System.getProperty("kafka.topic");
+        properties.setProperty("security.protocol", "SASL_SSL");
+        properties.setProperty("sasl.mechanism", "PLAIN");
+        properties.setProperty("sasl.jaas.config", jaasConfig);
+        properties.setProperty("bootstrap.servers", cluster);
+        myStore.initializeKafkaProducer(properties, topicName);
+        String newBook = "Invent and Wander";
+        String newAuthor = "Jeff Bezos";
+        Action<Author> action1 = Utilities.actionCreator(ACTION_SET_AUTHOR, new Author(newAuthor));
+        Action<Book> action2 = Utilities.actionCreator(ACTION_SET_BOOK, new Book(newBook));
+        myStore.dispatch(action1);
+        myStore.dispatch(action2);
+    }
+
+}
